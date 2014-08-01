b640f40a810a56bccf4d870261476b6b0cdad50c
K.-Michael Aye <kmichael.aye@gmail.com>
2014-08-01 02:26:00 -0700

    adding more metadata from the label into the object

--- pyuvis/qube.py
+++ pyuvis/qube.py
@@ -11,10 +11,23 @@ def get_labels(fname):
 
 class QUBE(object):
     def __init__(self, fname):
+        # file management
         self.file_id = os.path.splitext(fname)[0]
         self.label_fname = self.file_id + '.LBL'
         self.data_fname = self.file_id + '.DAT'
+
+        # read the data
         self.data1D = (np.fromfile(self.data_fname, '>H')).astype(np.uint16)
+
+        # label stuff
         self.labels = get_labels(self.label_fname)
+        self.LINE_BIN = self.labels['LINE_BIN']
+        self.BAND_BIN = self.labels['BAND_BIN']
         self.shape = eval(self.labels['QUBE']['CORE_ITEMS'])
+        self.line_range = (self.labels['UL_CORNER_LINE'],
+                           self.labels['LR_CORNER_LINE'])
+        self.band_range = (self.labels['UL_CORNER_BAND'],
+                           self.labels['LR_CORNER_BAND'])
+
+        # reshape the data with infos from label
         self.data = self.data1D.reshape(self.shape, order='F')
