d33bc2476cc5fb0d583e7e580ff03562cdef7f85
K.-Michael Aye <kmichael.aye@gmail.com>
2014-08-01 02:00:32 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,37 @@
+import ez_setup
+ez_setup.use_setuptools()
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = ['-v']
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+
+setup(
+    name = "pyuvis",
+    version = "0.1",
+    packages = find_packages(),
+
+    install_requires = ['pypds>=1.0'],
+    tests_require = ['pytest'],
+
+    cmdclass = {'test': PyTest},
+
+    #metadata
+    author = "K.-Michael Aye",
+    author_email = "kmichael.aye@gmail.com",
+    description = "Software tools for the Cassini UVIS instrument.",
+    license = "BSD",
+    keywords = "Cassini UVIS ",
+)
