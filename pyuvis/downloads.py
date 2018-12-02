3a9cb12de4b1c1f7bccd3ccac3ffaede48c54f2e
K.-Michael Aye <kmichael.aye@gmail.com>
2018-12-01 20:17:42 -0700

    first commit

---
+++ pyuvis/downloads.py
@@ -0,0 +1,9 @@
+import requests
+from urllib.request import urlretrieve
+
+url_template = ("http://pds-rings.seti.org/volumes/COUVIS_0xxx/"
+                "COUVIS_0001/index/index.tab")
+
+url_root = 'http://pds-rings.seti.org/volumes/COUVIS_0xxx/COUVIS_00'
+
+url_name = '/index/index.tab'
