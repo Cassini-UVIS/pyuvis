3ef6a17b1b1ce5438916770cbf07388ed1bab155
K.-Michael Aye <kmichael.aye@gmail.com>
2019-02-01 18:20:27 -0700

    temporarily defunc'ing get_last/first_minutes
    * due to pandas API change need to remove these functions for now

--- pyuvis/io.py
+++ pyuvis/io.py
@@ -7,68 +7,93 @@ import pandas as pd
 import pvl
 import seaborn as sns
 import xarray as xr
-from pandas import datetools
+from astropy import units as u
+
+# from pandas import datetools
 
 from .hsp_sensitivity import sens_df
 
 try:
     import ffmpy
+
     _FFMPY_INSTALLED = True
 except ImportError:
     _FFMPY_INSTALLED = False
 
 
 class QUBE(object):
-
     def __init__(self, fname):
         self.path = Path(fname)
         # file management
         self.file_id = self.path.stem
-        self.label_fname = self.path.with_suffix('.LBL')
-        self.data_fname = self.path.with_suffix('.DAT')
+        self.label_fname = self.path.with_suffix(".LBL")
+        self.data_fname = self.path.with_suffix(".DAT")
 
         # read the data
-        self.data1D = (np.fromfile(str(self.data_fname), '>H')).astype(np.uint16)
+        self.data1D = (np.fromfile(str(self.data_fname), ">H")).astype(np.uint16)
 
         # label stuff
         self.label = pvl.load(str(self.label_fname))
-        self.cubelabel = self.label['QUBE']
-        self.LINE_BIN = self.cubelabel['LINE_BIN']
-        self.BAND_BIN = self.cubelabel['BAND_BIN']
-        self.shape = tuple(self.cubelabel['CORE_ITEMS'])
-        self.line_range = (self.cubelabel['UL_CORNER_LINE'],
-                           self.cubelabel['LR_CORNER_LINE'])
-        self.band_range = (self.cubelabel['UL_CORNER_BAND'],
-                           self.cubelabel['LR_CORNER_BAND'])
+        self.cubelabel = self.label["QUBE"]
+        self.LINE_BIN = self.cubelabel["LINE_BIN"]
+        self.BAND_BIN = self.cubelabel["BAND_BIN"]
+        self.shape = tuple(self.cubelabel["CORE_ITEMS"])
+        self.line_range = (
+            self.cubelabel["UL_CORNER_LINE"],
+            self.cubelabel["LR_CORNER_LINE"] + 1,  # for numpy slicing + 1
+        )
+        self.band_range = (
+            self.cubelabel["UL_CORNER_BAND"],
+            self.cubelabel["LR_CORNER_BAND"] + 1,  # for numpy slicing + 1
+        )
 
         # reshape the data with infos from label
-        self.data = self.data1D.reshape(self.shape, order='F')
+        self.data = self.data1D.reshape(self.shape, order="F")[
+            slice(*self.band_range), slice(*self.line_range), :
+        ]
+
+    @property
+    def n_integrations(self):
+        return self.shape[-1]
+
+    @property
+    def waves(self):
+        return np.linspace(self.wave_min, self.wave_max, self.shape[0])
+
+    @property
+    def xarray(self):
+        da = xr.DataArray(self.data)
+
+
+class FUV_PDS(QUBE):
+    wave_min = 111.5 * u.nm
+    wave_max = 190.0 * u.nm
 
 
 class UVIS_NetCDF(object):
-
     def __init__(self, fname, freq):
         self.path = Path(fname)
         self.fname = str(self.path)
         self.ds = xr.open_dataset(self.fname)
         self.freq = freq
-        self.timestr = self.ds.start_time_str[:21] + '000'
+        self.timestr = self.ds.start_time_str[:21] + "000"
 
     @property
     def start_time(self):
         timestr = self.timestr
-        fmt = '%Y-%j %H:%M:%S.%f'
+        fmt = "%Y-%j %H:%M:%S.%f"
         return dt.datetime.strptime(timestr, fmt)
 
     @property
     def times(self):
-        times = pd.date_range(self.start_time, periods=self.n_integrations,
-                              freq=self.freq)
+        times = pd.date_range(
+            self.start_time, periods=self.n_integrations, freq=self.freq
+        )
         return times
 
     @property
     def n_integrations(self):
-        return self.ds['integrations'].size
+        return self.ds["integrations"].size
 
 
 class HSP(UVIS_NetCDF):
@@ -87,6 +112,7 @@ class HSP(UVIS_NetCDF):
     hsp = hsp('path', '1ms')
 
     """
+
     sensitivity = sens_df
 
     def __init__(self, fname, freq):
@@ -99,8 +125,9 @@ class HSP(UVIS_NetCDF):
     @property
     def times(self):
         """HSP times are different in that one needs to stack all rows."""
-        return pd.date_range(self.start_time, periods=self.n_integrations,
-                             freq=self.freq)
+        return pd.date_range(
+            self.start_time, periods=self.n_integrations, freq=self.freq
+        )
 
     @property
     def series(self):
@@ -115,16 +142,18 @@ class HSP(UVIS_NetCDF):
         return self.series / td.total_seconds()
 
     def get_last_minutes(self, min):
-        ind = self.series.index
-        return self.series[ind[-1] - datetools.Minute(min):]
+        return "Currently not implemented"
+        # ind = self.series.index
+        # return self.series[ind[-1] - datetools.Minute(min) :]
 
     def get_first_minutes(self, min):
-        ind = self.series.index
-        return self.series[:ind[0] + datetools.Minute(min)]
+        return "Currently not implemented."
+        # ind = self.series.index
+        # return self.series[: ind[0] + datetools.Minute(min)]
 
     @property
     def resampled(self):
-        return self.counts_per_sec.resample('1s')
+        return self.counts_per_sec.resample("1s")
 
     @property
     def cleaned_data_copy(self):
@@ -135,7 +164,7 @@ class HSP(UVIS_NetCDF):
         data[data > max] = np.nan
         return data
 
-    def plot_resampled_with_errors(self, binning='1s', ax=None):
+    def plot_resampled_with_errors(self, binning="1s", ax=None):
         data = self.cleaned_data_copy
         resampled = data.resample(binning)
         mean = resampled.mean()
@@ -143,11 +172,11 @@ class HSP(UVIS_NetCDF):
         if ax is None:
             fig, ax = plt.subplots()
         mean.plot(yerr=std, ax=ax)
-        ax.set_xlabel('Time')
-        ax.set_ylabel('Counts per second')
+        ax.set_xlabel("Time")
+        ax.set_ylabel("Counts per second")
         ax.set_title(f"Resampled to {binning} s")
 
-    def plot_relative_std(self, binning='1s', ax=None):
+    def plot_relative_std(self, binning="1s", ax=None):
         data = self.cleaned_data_copy
         resampled = data.resample(binning)
         mean = resampled.mean()
@@ -163,7 +192,7 @@ class HSP(UVIS_NetCDF):
         to_save = self.resampled.mean()
         tdindex = to_save.index - to_save.index[0]
         to_save.index = tdindex.seconds
-        to_save.to_csv(str(self.fname.with_suffix('.csv')))
+        to_save.to_csv(str(self.fname.with_suffix(".csv")))
 
     def __repr__(self):
         return self.ds.__repr__()
@@ -186,20 +215,20 @@ class FUV(UVIS_NetCDF):
     ========
     fuv = FUV('path', 'stellar', '1s')
     """
+
     wave_min = 111.5  # nm
     wave_max = 190.0  # nm
 
-    def __init__(self, fname, freq='1s'):
+    def __init__(self, fname, freq="1s"):
         super().__init__(fname, freq)
-        self.n_spec_bins = self.ds['spectral_dim_0'].size
-        self.waves = np.linspace(self.wave_min,
-                                 self.wave_max,
-                                 self.n_spec_bins)
-        self.ds['times'] = xr.DataArray(self.times.values, dims='integrations')
-        self.ds['wavelengths'] = xr.DataArray(self.waves, dims='spectral_dim_0')
-        self.ds = self.ds.swap_dims({'integrations': 'times',
-                                     'spectral_dim_0': 'wavelengths'})
-        self.ds = self.ds.rename({'window_0': 'counts', 'spatial_dim_0': 'pixels'})
+        self.n_spec_bins = self.ds["spectral_dim_0"].size
+        self.waves = np.linspace(self.wave_min, self.wave_max, self.n_spec_bins)
+        self.ds["times"] = xr.DataArray(self.times.values, dims="integrations")
+        self.ds["wavelengths"] = xr.DataArray(self.waves, dims="spectral_dim_0")
+        self.ds = self.ds.swap_dims(
+            {"integrations": "times", "spectral_dim_0": "wavelengths"}
+        )
+        self.ds = self.ds.rename({"window_0": "counts", "spatial_dim_0": "pixels"})
 
     @property
     def data(self):
@@ -207,13 +236,13 @@ class FUV(UVIS_NetCDF):
 
     @property
     def plotfolder(self):
-        f = self.path.parent / 'plots'
+        f = self.path.parent / "plots"
         f.mkdir(exist_ok=True)
         return f
 
     def save_spectograms(self):
-        sns.set_context('talk')
-        vmax = self.data.max()*1.05
+        sns.set_context("talk")
+        vmax = self.data.max() * 1.05
         for i, spec in enumerate(self.data):
             fig, ax = plt.subplots()
             spec.plot(ax=ax, vmax=vmax)
@@ -225,8 +254,8 @@ class FUV(UVIS_NetCDF):
         print("Saved spectrograms in", self.plotfolder)
 
     def _run_ffmpy(self, inputs, output_name):
-        opts = '-framerate 3 -y'
-        output_options = '-c:v libx264 -pix_fmt yuv420p'
+        opts = "-framerate 3 -y"
+        output_options = "-c:v libx264 -pix_fmt yuv420p"
         outputs = {str(self.plotfolder / output_name): output_options}
         ff = ffmpy.FF(global_options=opts, inputs=inputs, outputs=outputs)
         print("Running", ff.cmd_str)
@@ -236,14 +265,14 @@ class FUV(UVIS_NetCDF):
         if not _FFMPY_INSTALLED:
             print("ffmpy is not installed: 'pip install ffmpy'.")
             return
-        inputs = {str(self.plotfolder / 'spectogram_%04d.png'): None}
-        self._run_ffmpy(inputs, 'spectograms.mp4')
+        inputs = {str(self.plotfolder / "spectogram_%04d.png"): None}
+        self._run_ffmpy(inputs, "spectograms.mp4")
 
     def save_spectrums(self):
         "plotting spectrums over time summing all pixels"
-        sns.set_context('talk')
-        vmax = self.data.sum('pixels').max()*1.05
-        for i, spec in enumerate(self.data.sum('pixels')):
+        sns.set_context("talk")
+        vmax = self.data.sum("pixels").max() * 1.05
+        for i, spec in enumerate(self.data.sum("pixels")):
             fig, ax = plt.subplots()
             spec.plot()
             ax.set_ylim(0, vmax)
@@ -258,8 +287,8 @@ class FUV(UVIS_NetCDF):
         if not _FFMPY_INSTALLED:
             print("ffmpy is not installed: 'pip install ffmpy'.")
             return
-        inputs = {str(self.plotfolder / 'summed_spectrum_%04d.png'): None}
-        self._run_ffmpy(inputs, 'summed_spectrums.mp4')
+        inputs = {str(self.plotfolder / "summed_spectrum_%04d.png"): None}
+        self._run_ffmpy(inputs, "summed_spectrums.mp4")
 
     def __repr__(self):
         return self.ds.__repr__()
